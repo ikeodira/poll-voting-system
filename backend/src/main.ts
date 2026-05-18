6d5e59b2a4b3154d51596d466148796621052417
Ike Odira <odiraike8@gmail.com>
2026-05-18 09:29:52 +0100

    fix:include frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      "http://localhost:4200",
+      "https://poll-voting-system-pi.vercel.app",
+    ],
     credentials: true,
   });
   await app.listen(3000);
-  console.log('Backend running on http://localhost:3000/api');
+  console.log("Backend running on http://localhost:3000/api");
 }
 bootstrap();
