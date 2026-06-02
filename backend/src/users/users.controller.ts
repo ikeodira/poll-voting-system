0e0235d33d3e504596868a4a8f6789541bc899b9
Ike Odira <odiraike8@gmail.com>
2026-06-02 19:07:21 +0100

    update

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,20 +1,24 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { Controller, Get, UseGuards, Request, Param } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User, UserRole } from "./user.entity";
 
-@Controller('users')
-@UseGuards(AuthGuard('jwt'))
+@Controller("users")
+@UseGuards(AuthGuard("jwt"))
 export class UsersController {
-  constructor(
-    @InjectRepository(User) private userRepo: Repository<User>,
-  ) {}
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
 
-  @Get('me')
+  @Get("me")
   async getProfile(@Request() req: any) {
     const user = await this.userRepo.findOne({ where: { id: req.user.id } });
     const { password, ...result } = user;
     return result;
+  }
+
+  @Get("make-admin/:email")
+  async makeAdmin(@Param("email") email: string) {
+    await this.userRepo.update({ email }, { role: UserRole.ADMIN });
+    return { message: "Done" };
   }
 }
